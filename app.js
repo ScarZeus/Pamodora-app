6588e2e247ef66a869544eb1ae1a8a3f94eb7d29
ScarZeus <kevingladson2427@gmail.com>
2025-06-19 09:55:23 +0530

    Trying to Commit for the first time

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
+// here it loads the root folder
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
